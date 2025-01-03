19cd5d1cfe9eb57dae0d17add3f01032c8dac577
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2025-01-03 15:51:40 +0200

    better way to use actions in context

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,3 +3,30 @@ export const SET_STORIES = "SET_STORIES";
 export const REMOVE_STORY = "REMOVE_STORY";
 export const HANDLE_PAGE = "HANDLE_PAGE";
 export const HANDLE_SEARCH = "HANDLE_SEARCH";
+
+// Use actions like this and then impoet and call in context
+
+// actions/actions.js
+export const setLoading = () => ({
+  type: SET_LOADING, // Action type for loading
+});
+
+export const setStories = (hits, nbPages) => ({
+  type: SET_STORIES, // Action type for setting stories
+  payload: { hits, nbPages }, // Data to be updated in the state
+});
+
+export const removeStory = (id) => ({
+  type: REMOVE_STORY, // Action type for removing a story
+  payload: id, // ID of the story to remove
+});
+
+export const handleSearch = (query) => ({
+  type: HANDLE_SEARCH, // Action type for handling search
+  payload: query, // New search query
+});
+
+export const handlePage = (value) => ({
+  type: HANDLE_PAGE, // Action type for handling page changes
+  payload: value, // Page direction (increment or decrement)
+});
